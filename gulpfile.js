250d5f4bf697667db09be930a548f30880bcaef0
Sarrah Vesselov <sarchka1@gmail.com>
2015-02-25 09:49:32 -0500

    fixing tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp        = require("gulp");
 var less        = require("gulp-less");
-
+var bower       = require("main-bower-files");
+var concat      = require("gulp-concat");
 var lessPath     = "./less/**/*.less";
 
 
@@ -12,7 +13,13 @@ gulp.task("less", function() {
 
     .pipe(gulp.dest("./css"));
 
-})
+});
+
+gulp.task("bower:assets:js", function() { //adds important js files to new folder
+  gulp.src(bower({filter: "**/*.js"}))               // no need to upload all to git, just the imp. ones
+  .pipe(concat("vendor.js"))
+  .pipe(gulp.dest("./vendor/js"));
+});
 
 gulp.task("watch", function() {
   gulp.watch(lessPath, ["less"]);
